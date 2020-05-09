095e24e9f64822d1da4079bdfb1b68f0059ab6f4
Nik Sauer <nik.sauer@me.com>
2020-05-09 20:01:06 +0200

    complete lab 3

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-// import { exercise1 } from './lab2/exercise1';
-// import { exercise2 } from './lab2/exercise2';
+import { exercise1 as lab2Exercise1 } from './lab2/exercise1';
+import { exercise2 as lab2Exercise2 } from './lab2/exercise2';
 
-import { exercise2 } from './lab3/exercise2';
+import { exercise2 as lab3Exercise2 } from './lab3/exercise2';
 
 async function main() {
-  // await exercise1();
-  // await exercise2();
+  await lab2Exercise1();
+  await lab2Exercise2();
 
-  await exercise2();
+  await lab3Exercise2();
 }
 
 (async () => {
